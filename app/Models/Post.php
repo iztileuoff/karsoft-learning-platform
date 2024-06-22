72ff5a045adec60538beb4a394af0051080ad222
iztileuoff <bektemir0714@gmail.com>
2024-06-22 13:21:38 +0500

    add translatable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,12 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Translatable\HasTranslations;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
 
     protected $fillable = [
+        'name',
+    ];
+
+    public array $translatable = [
         'name',
     ];
 
