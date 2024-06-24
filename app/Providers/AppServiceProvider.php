dd4601da17b4ef980453613249225e80a2a78ba7
iztileuoff <bektemir0714@gmail.com>
2024-06-24 09:57:17 +0500

    add response macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Response::macro('success', function ($data, $message = null) {
+            return response()->json([
+                'message' => $message != null ? $message : __('http-statuses.200'),
+                'data' => $data
+            ]);
+        });
+
+        Response::macro('ok', function ($message = null) {
+            return response()->json([
+                'message' => $message != null ? $message : __('http-statuses.200'),
+            ]);
+        });
+
     }
 
     /**
