32fe4a174045975b1e19ad081371fd4d1f3b0543
iztileuoff <bektemir0714@gmail.com>
2024-06-24 11:27:08 +0500

    add Region and District model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Database\Seeders\V1\PostSeeder;
+use Database\Seeders\V1\RegionSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
+            RegionSeeder::class,
             PostSeeder::class,
         ]);
     }
