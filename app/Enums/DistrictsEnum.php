62163460d7ab14dee4f553968498ca9357d3fe5e
iztileuoff <bektemir0714@gmail.com>
2024-06-24 11:31:20 +0500

    add Region and District model

---
+++ app/Enums/DistrictsEnum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Enums;
+
+enum DistrictsEnum: int
+{
+    case Amudaryo = 1;
+    case Beruniy = 2;
+    case Kegayli = 3;
+    case Qanlikol = 4;
+    case Qaraozak = 5;
+    case Qongirot = 6;
+    case Moynoq = 7;
+    case NukusTuman = 8;
+    case Nukus = 9;
+    case Taxtakopir = 10;
+    case Tortkol = 11;
+    case Xojayli = 12;
+    case Chimboy = 13;
+    case Shumanay = 14;
+    case Ellikqala = 15;
+    case Taxiatosh = 16;
+    case Bozatov = 17;
+}
