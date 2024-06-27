3311dcc3cbc7b3a2d06a8c321e4329be27b46ecf
iztileuoff <bektemir0714@gmail.com>
2024-06-27 14:10:47 +0500

    change from ulid to id

--- database/migrations/2024_06_24_105556_create_textbooks_table.php
+++ database/migrations/2024_06_24_105556_create_textbooks_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('textbooks', function (Blueprint $table) {
-            $table->ulid('id')->primary();
+            $table->id();
             $table->string('name');
             $table->text('description');
             $table->foreignId('degree_id')->nullable()->constrained()->nullOnDelete();
