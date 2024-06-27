233ba8e6c03477687df04b87de77cd32f2c3e0ff
iztileuoff <bektemir0714@gmail.com>
2024-06-27 15:09:38 +0500

    add district_id

--- app/Http/Requests/Api/V1/Front/UpdateProfileRequest.php
+++ app/Http/Requests/Api/V1/Front/UpdateProfileRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api\V1\Front;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -15,7 +16,10 @@ class UpdateProfileRequest extends FormRequest
             'phone' => ['string', 'regex:/^998([0-9][012345789]|[0-9][125679]|7[01234569])[0-9]{7}$/', Rule::unique('users', 'phone')->ignore($this->user())],
             'password' => ['string', 'min:8', 'max:64'],
             'post_id' => [Rule::exists('posts', 'id')],
-            'school_id' => [Rule::exists('schools', 'id')],
+            'district_id' => ['required_with:school_id', Rule::exists('districts', 'id')],
+            'school_id' => [Rule::exists('schools', 'id')->where(function (Builder $query) {
+                $query->where('district_id', $this->district_id);
+            })],
         ];
     }
 
