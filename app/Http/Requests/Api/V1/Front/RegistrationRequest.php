2e9df70321e2407d119144c849252bb488707e96
iztileuoff <bektemir0714@gmail.com>
2024-06-27 11:48:49 +0500

    change password validation rule

--- app/Http/Requests/Api/V1/Front/RegistrationRequest.php
+++ app/Http/Requests/Api/V1/Front/RegistrationRequest.php
@@ -13,7 +13,7 @@ class RegistrationRequest extends FormRequest
             'first_name' => ['required', 'string', 'max:200'],
             'last_name' => ['required', 'string', 'max:200'],
             'phone' => ['required', 'string', 'regex:/^998([0-9][012345789]|[0-9][125679]|7[01234569])[0-9]{7}$/', Rule::unique('users', 'phone')],
-            'password' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'max:64'],
             'school_id' => ['required', Rule::exists('schools', 'id')],
             'post_id' => ['required', Rule::exists('posts', 'id')],
             'is_admin' => ['nullable'],
