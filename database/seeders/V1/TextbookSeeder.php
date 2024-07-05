96b386511bf1a4f74d2cf0eb70b14feba4c11bcd
iztileuoff <bektemir0714@gmail.com>
2024-07-05 17:39:21 +0500

    add images for textbooks

--- database/seeders/V1/TextbookSeeder.php
+++ database/seeders/V1/TextbookSeeder.php
@@ -4,16 +4,10 @@ namespace Database\Seeders\V1;
 
 use App\Enums\DegreesEnum;
 use App\Enums\LanguagesEnum;
-use App\Models\Author;
-use App\Models\Info;
-use App\Models\Post;
 use App\Models\Textbook;
 use Illuminate\Database\Seeder;
-
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
-
-use function Symfony\Component\Translation\t;
 
 class TextbookSeeder extends Seeder
 {
@@ -30,7 +24,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G5_QQ_18.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G5_QQ_18.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_16-59-37.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "5-sinf. Informatika va axborot texnologiyalari",
@@ -39,7 +38,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G05_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G05_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_16-59-37.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "6-klass. Informatika hám informaciyalıq texnologiyaları",
@@ -48,7 +52,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G6_QQ_17.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G6_QQ_17.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-00-44.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "6-sinf. Informatika va axborot texnologiyalari",
@@ -57,7 +66,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G06_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G06_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-00-44.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "7-klass. Informatika hám informaciyalıq texnologiyaları",
@@ -66,7 +80,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G7_QQ_17.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G7_QQ_17.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-01-27.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "7-sinf. Informatika va axborot texnologiyalari",
@@ -75,7 +94,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G07_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G07_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-01-27.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "8-klass. Informatika hám informaciyalıq texnologiyaları",
@@ -84,7 +108,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G8_QQ_17.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G8_QQ_17.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-02-10.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "8-sinf. Informatika va axborot texnologiyalari",
@@ -93,7 +122,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G08_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G08_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-02-10.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "9-klass. Informatika hám informaciyalıq texnologiyaları",
@@ -102,7 +136,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G9_QQ_18.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G9_QQ_18.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-02-49.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "9-sinf. Informatika va axborot texnologiyalari",
@@ -111,7 +150,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G09_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G09_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-02-49.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "10-klass. Informatika hám informaciyalıq texnologiyaları",
@@ -120,7 +164,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G10-11_QQ_18.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G10-11_QQ_18.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-03-54.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "10-sinf. Informatika va axborot texnologiyalari",
@@ -129,7 +178,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G10-11_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G10-11_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-03-54.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "11-klass. Informatika hám informaciyalıq texnologiyaları",
@@ -138,7 +192,12 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::kaa,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G10-11_QQ_18.04.2024.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/kaa/ICT_STB_G10-11_QQ_18.04.2024.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-03-54.png'))->toMediaCollection(
+            'image'
+        );
 
         $textbook = Textbook::create([
             'name' => "11-sinf. Informatika va axborot texnologiyalari",
@@ -147,6 +206,11 @@ class TextbookSeeder extends Seeder
             'language' => LanguagesEnum::uz,
         ]);
 
-        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G10-11_STB.pdf'))->toMediaCollection('file');
+        $textbook->copyMedia(storage_path('app/textbooks/uz/ICT_G10-11_STB.pdf'))->toMediaCollection(
+            'file'
+        );
+        $textbook->copyMedia(storage_path('app/textbooks/images/image_2024-07-05_17-03-54.png'))->toMediaCollection(
+            'image'
+        );
     }
 }
