56d1787d1afda25686a8032f88c4c00dc5c2b3b7
iztileuoff <bektemir0714@gmail.com>
2024-07-05 10:27:31 +0500

    refactor code

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -46,21 +46,17 @@ class Test extends Model
     public function getOnlyNumberAndTextQuestionsOptions(): ?\Illuminate\Support\Collection
     {
         if ($this->data_questions != null) {
-            return collect($this->data_questions)->map(function ($question) {
-                return [
-                    'id' => $question['id'],
-                    'quiz_id' => $question['quiz_id'],
-                    'text' => $question['text'],
-                    'image_url' => $question['image_url'],
-                    'options' => collect($question['options'])->map(function ($option) {
-                        return [
-                            'number' => $option['number'],
-                            'text' => $option['text'],
-                            'image_url' => $option['image_url'],
-                        ];
-                    })
-                ];
-            });
+            return collect($this->data_questions)->map(fn($question) => [
+                'id' => $question['id'],
+                'quiz_id' => $question['quiz_id'],
+                'text' => $question['text'],
+                'image_url' => $question['image_url'],
+                'options' => collect($question['options'])->map(fn($option) => [
+                    'number' => $option['number'],
+                    'text' => $option['text'],
+                    'image_url' => $option['image_url'],
+                ])
+            ]);
         }
 
         return null;
