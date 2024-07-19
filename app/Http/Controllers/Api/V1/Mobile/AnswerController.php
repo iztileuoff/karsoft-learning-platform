05d6318a83afc6fc4d9870b3c313978514029822
iztil <bektemir0714@gmail.com>
2024-07-19 16:18:54 +0500

    fix bug

--- app/Http/Controllers/Api/V1/Mobile/AnswerController.php
+++ app/Http/Controllers/Api/V1/Mobile/AnswerController.php
@@ -6,12 +6,17 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Mobile\AnswerRequest;
 use App\Http\Resources\V1\Front\TestAnswerResource;
 use App\Models\Test;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class AnswerController extends Controller
 {
     public function index(AnswerRequest $request)
     {
         $test = Test::find($request->input('test_id'));
+
+        if ($test->finished_at != null) {
+            throw new AccessDeniedHttpException();
+        }
 
         return new TestAnswerResource($test);
     }
