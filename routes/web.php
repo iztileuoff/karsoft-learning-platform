5d6640d2857a9c82f1894c76b64f064f86fa50b1
iztil <bektemir0714@gmail.com>
2024-09-16 16:10:32 +0500

    add admin

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,20 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 Route::get('/', function () {
     throw new NotFoundHttpException();
 })->name('login');
+
+Route::get('/test', function () {
+    \App\Models\User::create([
+        'first_name' => 'Admin',
+        'last_name' => 'User',
+        'is_admin' => true,
+        'phone' => '998999999999',
+        'email' => null,
+        'password' => '12344321',
+        'post_id' => 1,
+        'district_id' => 9,
+        'school_id' => 1,
+        'google_id' => null,
+        'telegram_id' => null,
+    ]);
+});
+
