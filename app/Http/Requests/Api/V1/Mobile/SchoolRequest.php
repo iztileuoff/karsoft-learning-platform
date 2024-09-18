68a34c6e81494ce95688b1ba7ca5f196a74cc502
iztil <bektemir0714@gmail.com>
2024-09-18 16:47:06 +0500

    fix bug

--- app/Http/Requests/Api/V1/Mobile/SchoolRequest.php
+++ app/Http/Requests/Api/V1/Mobile/SchoolRequest.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Requests\Api\V1\Mobile;
 
-use App\Enums\DistrictsEnum;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class SchoolRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'district_id' => ['required', 'int', Rule::enum(DistrictsEnum::class)],
+            'district_id' => ['required', 'int'],
         ];
     }
 
